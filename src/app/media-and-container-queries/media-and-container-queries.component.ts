4bcde12352c4fd72422ae27c58e30488015d8ba6
bala <sr.bala.kkl@gmail.com>
2025-08-22 08:52:34 +0530

    NEW: MediaAndContainerQuery

---
+++ src/app/media-and-container-queries/media-and-container-queries.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-and-container-queries',
+  standalone: true,
+  imports: [],
+  templateUrl: './media-and-container-queries.component.html',
+  styleUrl: './media-and-container-queries.component.css'
+})
+export class MediaAndContainerQueriesComponent {
+
+}
